032462b4c277e0fbed4028b1dfb54897ce94dc31
Thais Santos <thais.santos@vilt-group.com>
2017-04-06 02:26:12 -0300

    Removing Unused button

--- app/src/main/java/com/natura/teste/thaissantos/makeitup/DisplayCatalogActivity.java
+++ app/src/main/java/com/natura/teste/thaissantos/makeitup/DisplayCatalogActivity.java
@@ -2,11 +2,8 @@ package com.natura.teste.thaissantos.makeitup;
 
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -24,15 +21,6 @@ public class DisplayCatalogActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         dbhelper = new DataBaseHelper(this);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         Bundle extras = getIntent().getExtras();
 
